7050259f8a2d0ffbbc566580835bc14ae1e42675
Roshan Nasseri <roshan.sep26@gmail.com>
2021-05-27 23:21:37 +0000

    add links to login register & logout to the nav bar, verify users & comfirmation pass is the same & add routs for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
