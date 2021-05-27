959f26818f8b17dd47ce5538b9f0c7f43b18f13f
Roshan Nasseri <roshan.sep26@gmail.com>
2021-05-27 20:54:30 +0000

    Add admin categories/ authentication & index view for categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,13 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    @categories = Category.order(:id).all
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
